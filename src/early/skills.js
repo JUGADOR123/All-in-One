d16e1f087341f342f458bf516a29cee2188b4015
Jugador123 <53621532+JUGADOR123@users.noreply.github.com>
2020-11-24 09:36:15 -0600

    Add files via upload

--- src/early/skills.js
+++ src/early/skills.js
@@ -1,21 +1,24 @@
 exports.mod = () => {
     logger.logInfo("[MOD] All in one Module");
     let settings = require("../../settings.json");
-    if (settings.player.skillMultiplier > 1) {
+    if (settings.player.enableskillMultiplier == true) {
         let base = fileIO.readParsed(db.cacheBase.globals);
         base.config.SkillProgressRate = settings.player.skillMultiplier;
         base.config.WeaponSkillProgressRate = settings.player.skillMultiplier;
+        base.config.SkillMinEffectiveness = 10;
+        base.configSkillFatiguePerPoint = 0;
+        base.config.SkillFreshEffectiveness = 10;
+
         fileIO.write(db.cacheBase.globals, base);
-        logger.logSuccess("[Mod Aio] Skill multiplier increased to "+settings.player.skillMultiplier);
+        logger.logSuccess("[Mod Aio] Skill multiplier increased to " + settings.player.skillMultiplier);
     }
-    if (settings.player.skillFatigue == false) {
+    else {
         let base = fileIO.readParsed(db.cacheBase.globals);
-        base.SkillMinEffectiveness = 2;
-        base.SkillFatiguePerPoint = 0;
-        base.SkillFreshEffectiveness = 10;
-        fileIO.write(db.cacheBase.globals, base);
-        logger.logSuccess("[Mod Aio] Successfullu removed skill fatigue");
+        base.config.SkillProgressRate = 0.6;
+        base.config.WeaponSkillProgressRate = 1;
+        base.SkillMinEffectiveness = 0.0001;
+        base.SkillFreshEffectiveness = 1.3;
+        base.SkillFatiguePerPoint = 0.5;
     }
-    
 
 }
