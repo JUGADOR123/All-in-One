1a4478a6a7c2c8c545abd5b7b91d75b88527ae9c
JUGADOR123 <momocordova@gmail.com>
2020-12-03 17:53:29 -0600

    Update to 2.0.8

--- src/timer.js
+++ src/timer.js
@@ -1,13 +1,11 @@
 exports.mod = () => {
     let settings = require("../settings.json");
     if (settings.gameplay.enablelongerraids==true) {
-        let base = fileIO.readParsed(db.user.cache.locations)
-        for (let map in base) {
-            base[map].base.exit_access_time = settings.gameplay.raidTimer
-            base[map].base.escape_time_limit = settings.gameplay.raidTimer
+        let file = fileIO.readParsed(db.user.cache.locations)
+        for (let map in file) {
+            file[map].base.exit_access_time = settings.gameplay.raidTimer;
         }
-        fileIO.write(db.user.cache.locations, base);
+        fileIO.write(db.user.cache.locations, file);
+        logger.logSuccess("[Mod Aio] Raids have been extended");
     }
-    logger.logSuccess("[Mod Aio] Raids have been extended");
-
 }
